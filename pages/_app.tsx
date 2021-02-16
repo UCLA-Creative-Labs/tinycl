b803d61fba58fc02187ba6f21c75dd01b4b4eaa1
Bryan Pan <bryanpan342@gmail.com>
2021-02-16 11:52:02 -0800

    chore: add scripts for linters and other checks

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,10 @@
-import '../styles/globals.css'
+import {AppProps} from 'next/app';
+import React from 'react';
+import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
