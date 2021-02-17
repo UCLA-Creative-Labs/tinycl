f4c5989d3c845aa26564a4826a37fa187315f683
Bryan Pan <bryanpan342@gmail.com>
2021-02-17 14:10:12 -0800

    fix: typo in readme (#1)
    * typo in the readme fixed
    * testing mergify and all other checks

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import {AppProps} from 'next/app';
 import React from 'react';
-import '../styles/globals.css';
+import '../styles/globals.scss';
 
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
