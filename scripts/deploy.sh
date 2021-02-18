f825a205625dbbc3392d067ec8e4f8e8a7e2e4f9
Bryan Pan <bryanpan342@gmail.com>
2021-02-18 12:05:24 -0800

    fix: remove faulty arg in deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,7 +7,7 @@ echo "Starting build..."
 IMAGE="creativelabsucla/tinycl"
 GIT_VERSION=$(git describe --always --abbrev --tags --long)
 
-docker build --build-arg -t ${IMAGE}:${GIT_VERSION} .
+docker build -t ${IMAGE}:${GIT_VERSION} .
 docker tag ${IMAGE}:${GIT_VERSION} ${IMAGE}:latest
 
 echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
