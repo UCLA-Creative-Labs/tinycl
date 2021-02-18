1e316833691ad9e189fa7cfeff56a5ba0dc04f3e
Bryan Pan <bryanpan342@gmail.com>
2021-02-18 13:28:11 -0800

    yarn linter

--- utils/firebase.ts
+++ utils/firebase.ts
@@ -1,13 +1,13 @@
 import firebase from 'firebase';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB4j5jobpAmTA1qEONI9YQVFvzZTVK_Epg",
-  authDomain: "tinycl-fb2a5.firebaseapp.com",
-  projectId: "tinycl-fb2a5",
-  storageBucket: "tinycl-fb2a5.appspot.com",
-  messagingSenderId: "166139891805",
-  appId: "1:166139891805:web:ccc8c77311562dc7e23db4",
-  measurementId: "G-F8EBLSNLSV"
+  apiKey: 'AIzaSyB4j5jobpAmTA1qEONI9YQVFvzZTVK_Epg',
+  authDomain: 'tinycl-fb2a5.firebaseapp.com',
+  projectId: 'tinycl-fb2a5',
+  storageBucket: 'tinycl-fb2a5.appspot.com',
+  messagingSenderId: '166139891805',
+  appId: '1:166139891805:web:ccc8c77311562dc7e23db4',
+  measurementId: 'G-F8EBLSNLSV',
 };
 
 if (!firebase.apps.length) {
