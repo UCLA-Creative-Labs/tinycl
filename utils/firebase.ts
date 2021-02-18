445afcae90a5a7fcf358e639ddb40f4f1c0b3153
Bryan Pan <bryanpan342@gmail.com>
2021-02-18 13:40:34 -0800

    fix: remove faulty arg in deploy script (#17)
    * expose firebase api (we can do this for a variety of reasons [see here](https://stackoverflow.com/questions/37482366/is-it-safe-to-expose-firebase-apikey-to-the-public)
    * remove bad argument in `docker build` command

--- utils/firebase.ts
+++ utils/firebase.ts
@@ -1,14 +1,13 @@
 import firebase from 'firebase';
 
 const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  databaseUrl: process.env.DATABASE_URL,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID,
-  measurementId: process.env.MEASUREMENT_ID,
+  apiKey: 'AIzaSyB4j5jobpAmTA1qEONI9YQVFvzZTVK_Epg',
+  authDomain: 'tinycl-fb2a5.firebaseapp.com',
+  projectId: 'tinycl-fb2a5',
+  storageBucket: 'tinycl-fb2a5.appspot.com',
+  messagingSenderId: '166139891805',
+  appId: '1:166139891805:web:ccc8c77311562dc7e23db4',
+  measurementId: 'G-F8EBLSNLSV',
 };
 
 if (!firebase.apps.length) {
