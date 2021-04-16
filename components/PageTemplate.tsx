d2c81a58e5a1779fc359d07ca23922053033ed3e
Isha G <igonugun@gmail.com>
2021-04-16 14:55:23 -0700

    fix: router removed from PageTemplate

--- components/PageTemplate.tsx
+++ components/PageTemplate.tsx
@@ -2,12 +2,10 @@ import React from 'react';
 import {Link, PageProps} from '../utils';
 import Layout from './Layout';
 import styles from '../styles/Page.module.scss';
-import { useRouter } from 'next/router';
 
 export default function PageTemplate(props: PageProps): JSX.Element {
 
-  const {pageName, links, redirect} = props;
-  const router = useRouter();
+  const {pageName, links} = props;
 
   return (
     <Layout
