26c6f1a088ce15fd0af2b69d13e25fd14338a7dc
Bryan Pan <bryanpan342@gmail.com>
2021-04-24 21:41:53 -0700

    redirect to home

--- scripts/prebuild.js
+++ scripts/prebuild.js
@@ -25,7 +25,7 @@ const main = async () => {
   const redirects = data.linkCollection.items.reduce((acc, {redirectPath, url}) => {
     return `${acc}/${redirectPath} ${url}\n`;
   }, '');
-  writeFileSync(path.resolve(__dirname, '../_redirects'), redirects);
+  writeFileSync(path.resolve(__dirname, '../_redirects'), `${redirects}\n/* / 200`);
 }
 
 main();
