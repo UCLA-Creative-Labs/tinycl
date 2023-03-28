52fe435bdd23d0cfd3f04760ee198882809db0b0
Darren <darrenzhang22@gmail.com>
2023-03-27 22:26:12 -0700

    Set redirect path back to original

--- scripts/prebuild.js
+++ scripts/prebuild.js
@@ -25,9 +25,8 @@ const main = async () => {
   const redirects = data.linkCollection.items.reduce((acc, {redirectPath, url}) => {
     return `${acc}/${redirectPath} ${url}\n`;
   }, '');
-  writeFileSync(path.resolve(__dirname, '../out/_redirects'), redirects);
-  console.log("3: redirect Path: "+path.resolve(__dirname, '../_redirects'));
-  console.log("3: redirect Path: "+path.resolve(__dirname, '../out/_redirects'));
+  writeFileSync(path.resolve(__dirname, '../_redirects'), redirects);
+  console.log("1: redirect Path: "+path.resolve(__dirname, '../_redirects'));
 }
 
 main();
