8973a9df14cd7c244c05eb6048f93d096d8f8b17
ShawnL57 <110152943+ShawnL57@users.noreply.github.com>
2026-03-04 23:16:19 -0800

    feat: update index.tsx to fetch links from supabase

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,21 @@
 import { GetStaticProps } from 'next';
 import React from 'react';
 import Layout from '../components/Layout';
-import {fetchContentful, PageProps} from '../utils';
-import {pageQuery} from '../utils/index';
+import { fetchLinks, PageProps } from '../utils';
 
-export default function Home({pageName, links, pages}: PageProps): JSX.Element {
-
+export default function Home({ pageName, links, pages }: PageProps): JSX.Element {
   return (
     <Layout pageName={pageName} links={links} pages={pages} />
   );
 }
 
 export const getStaticProps: GetStaticProps = async () => {
-  const data = await fetchContentful(pageQuery);
-  const pages = data?.pageCollection?.items.map(({pageName}) => pageName);
-  const page = data.pageCollection.items.find(({pageName}) => pageName === 'home');
-  return {props: {
-    pageName: '',
-    links: page?.linksCollection?.items,
-    pages,
-  }};
+  const links = await fetchLinks();
+  return {
+    props: {
+      pageName: '',
+      links,
+      pages: [],
+    },
+  };
 };
